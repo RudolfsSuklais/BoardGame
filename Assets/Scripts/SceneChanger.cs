088ee4794821ef4c9abb01f23525119160503751
Rudolfs <ruudisrudolfs@gmail.com>
2025-12-04 08:59:06 +0200

    Added Start button logic, now able to switch to character select

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void CloseGame()
+    {
+        StartCoroutine(Quit());
+    }
+
+    IEnumerator Quit()
+    {
+        yield return new WaitForSeconds(0.8f);
+        Application.Quit();
+    }
+}
